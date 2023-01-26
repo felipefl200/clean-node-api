d73a0b6629c0616ff6a2136807e1ed0bc36af3b9
felipefl200 <felipefl200@gmail.com>
2023-01-26 14:41:30 -0300

    feat: ensure SignUpController return 400 if no password or passwordConfirmation is not provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -17,9 +17,7 @@ describe('Signup Controller', () => {
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new MissingParamError('name'))
     })
-})
 
-describe('Signup Controller', () => {
     test('Should return 400 if no email is provided', () => {
         const sut = new SignUpController()
         const httpRequest = {
@@ -35,4 +33,37 @@ describe('Signup Controller', () => {
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new MissingParamError('email'))
     })
+
+    test('Should return 400 if no email is provided', () => {
+        const sut = new SignUpController()
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                // email: 'any_email@mail.com',
+                password: 'any_password',
+                passwordConfirmation: 'any_password'
+
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamError('email'))
+    })
+
+    test('Should return 400 if no passwordConfirmation is provided', () => {
+        const sut = new SignUpController()
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                email: 'any_email@mail.com',
+                password: 'any_password',
+                // passwordConfirmation: 'any_password'
+
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
+    })
 })
+
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,14 +3,24 @@ import { HttpResponse, HttpRequest } from '../protocols/http'
 import { badRequest } from '../helpers/http-helpers'
 
 export class SignUpController {
-    handle(httpRequest: HttpRequest): HttpResponse {
-        if (!httpRequest.body.name) {
-            return badRequest(new MissingParamError('name'))
-        }
+    handle(httpRequest: HttpRequest): HttpResponse | any {
 
-        if (!httpRequest.body.email) {
-            return badRequest(new MissingParamError('email'))
+        // if (!httpRequest.body.name) {
+        //     return badRequest(new MissingParamError('name'))
+        // }
+
+        // if (!httpRequest.body.email) {
+        //     return badRequest(new MissingParamError('email'))
+        // }
+
+
+
+        const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+
+        for (const field of requiredFields) {
+            if (!httpRequest.body[field]) {
+                return badRequest(new MissingParamError(field))
+            }
         }
-        return badRequest()
     }
 }
